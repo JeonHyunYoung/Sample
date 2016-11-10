b3b5a9c4ba5baee5f81806984577dc95d6e0b41e
JeonHyunYoung <hyeon454@naver.com>
2016-11-10 22:17:37 +0900

    Add files via upload

---
+++ aoptx/src/main/java/com/hyun/persistence/MessageDAO.java
@@ -0,0 +1,9 @@
+package com.hyun.persistence;
+
+import com.hyun.domain.MessageVO;
+
+public interface MessageDAO {
+	public void create(MessageVO vo) throws Exception;
+	public MessageVO readMessage(Integer mid) throws Exception;
+	public void updateState(Integer mid) throws Exception;
+}
